f28e6aa2c48d6214ea7a901cf8ea560771dec9ed
safiya siddiqui <safiyaqazi517@gmail.com>
2023-08-28 19:00:47 +0530

    density intern

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-
-import './App.css';
-import Navbar from './Navbar';
-import About from './About';
-import Description from './Description';
-import Slides from './Slides';
-import Metaverse from './Metaverse';
-import MetaWorld from './MetaWorld';
-import Global from './Global';
-import Blog from './Blog';
-import Metaversend from './Metaversend';
+import "./App.css";
+import Navbar from "./Navbar";
+import About from "./About";
+import Description from "./Description";
+import Slides from "./Slides";
+import Metaverse from "./Metaverse";
+import MetaWorld from "./MetaWorld";
+import Global from "./Global";
+import Blog from "./Blog";
+import Metaversend from "./Metaversend";
 function App() {
   return (
     <div className="App">
-    <Navbar/>
-    <About/>
-<Description/>
-   <Slides/>
-<Metaverse/>
-<MetaWorld/>
-<Global/>
-<Blog/>
-<Metaversend/>
+      <Navbar />
+      <About />
+      <Description />
+      <Slides />
+      <Metaverse />
+      <MetaWorld />
+      <Global />
+      <Blog />
+      <Metaversend />
+      {/* <Offer/> */}
     </div>
   );
 }
